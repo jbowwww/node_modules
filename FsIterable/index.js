ddbf1f508b7e4983d1b7726e04ede81f1a4d008c
Comment <jbowwww@gmail.com>
2019-08-21 12:35:06 +0800

    Initial commit after restarting repo to contain all of my node_modules

---
+++ FsIterable/index.js
@@ -0,0 +1,113 @@
+"use strict";
+
+const obj /*{ assignDefaults, inspect, promisify }*/ = require('../obj');
+const inspect = obj.inspect;
+const promisifyObject = o => Object.keys(o).reduce((a, k) =>
+	Object.defineProperty(a, k, { writeable: true, enumerable: true, value: o[k] instanceof Function ? obj.promisify(o[k]) : o[k] }), {});
+const nodeFs = promisifyObject(require('fs'));
+const nodePath = require('path');
+const stream = new require('stream');
+stream.finished = obj.promisify(stream.finished);
+var pipeline = obj.promisify(stream.pipeline);
+const log = require('debug')('FsIterable');
+log.info = log.extend('info');
+log.warn = log.extend('warn');
+
+module.exports = FsIterable;
+
+function FsIterable(options) {
+	if (!(this instanceof FsIterable)) {
+		return new FsIterable(options);
+	} else if (typeof options === 'string') {
+		options = { path: options };
+	}
+	
+	this.errors = [];
+	this.items = obj.inspect.withGetters([], () => 'Array[' + this.items.length + ']');
+	this.itemIndex = 0;
+	const fsIterable = this;
+	this.options = options = obj.assignDefaults(options, {
+		path: nodePath.resolve(options.path || '.'),
+		maxDepth: 1,
+		filter: item => true,
+		handleError(err) { fsIterable.errors.push(err); log/*.warn*/(`Error!: ${err/*.stack*/}`); }
+	});
+	this.count = obj.inspect.withGetters({
+		file: 0,
+		dir: 0,
+		unknown: 0,
+		get all() { return this.file + this.dir + this.unknown; },
+		doneCounting: false
+ 	});
+	log(`FsIter(${obj.inspect(this.options, { compact: false })})`);//: this=${this}`);
+
+	this.progress = obj.inspect.withGetters({
+		get total() { return  fsIterable.count.all; },
+		get current() { return fsIterable.itemIndex; },
+		get progress() { return this.total === 0 ? 0 : 100 * fsIterable.itemIndex / this.total; },
+		get done() { return this.doneCounting && this.current === this.total; },
+		get doneCounting() { return fsIterable.count.doneCounting; }
+	}, { progress: v => '' + v + '%' });
+
+	this._fsIterateInnerCalls = 0;
+	this._rootPathDepth = this.options.path.split(nodePath.sep).length;
+
+	// Start iterator before asyncIterator is even invoked
+	// (async() => {
+		const innerIter = fsIterateInner(this.options.path);
+		this[Symbol.asyncIterator] = async function* () {
+			this.itemIndex = 0;
+			log(`asyncIterator! this=${inspect(this)}`);
+			for await(const item of innerIter) {//this._fsIterateInnerCalls > 0 || this.itemIndex < this.items.length) {
+				log(`item: ${item.fileType}: '${item.path}'`);
+				yield item;
+				this.itemIndex++;
+			}
+		};
+	// })();
+
+	async function* fsIterateInner(path) {
+		try {
+			const item = await createItem(path);
+			fsIterable._fsIterateInnerCalls++;
+			// log('fsIterateInner: item=%s', inspect(item, { compact: true }));
+			const currentDepth = item.pathDepth - fsIterable._rootPathDepth;
+			log(`newItem: ${item.fileType}: currentDepth=${currentDepth} '${item.path}'`);
+			yield item;	//fsIterable.items.push(item);
+			if (item.fileType === 'dir' && (fsIterable.options.maxDepth === 0 || currentDepth < fsIterable.options.maxDepth)) {
+				const names = await nodeFs.readdir(item.path)
+				const filteredNames = names.filter(fsIterable.options.filter || (() => true));
+				log('%d entries, %d matching filter at depth=%d in dir:%s, progress=%s', names.length, filteredNames.length, currentDepth, item.path, inspect(fsIterable.progress));
+				const paths = filteredNames.map(name => nodePath.join(item.path, name))
+				// const items = await Promise.all();
+				// yield items;
+				for await (const newItem of paths) {
+					yield* fsIterateInner(newItem);
+				}
+			}
+		} catch (e) {
+			fsIterable.options.handleError(e);
+		} finally {
+			if (--fsIterable._fsIterateInnerCalls === 0) {
+				fsIterable.count.doneCounting = true;
+			};
+		}
+	}
+
+	async function createItem(path) {
+		const item = obj.inspect.withGetters({
+			path: /*nodePath.resolve*/(path),
+			stats: await nodeFs.lstat(path),
+			get fileType() { return this.stats.isDirectory() ? 'dir' : this.stats.isFile() ? 'file' : 'unknown'; },
+			get pathDepth() { return this.path.split(nodePath.sep).length; },
+			get extension() {
+				var n = this.path.lastIndexOf('.');
+				var n2 = Math.max(this.path.lastIndexOf('/'), this.path.lastIndexOf('\\'));
+				return (n < 0 || (n2 > 0 && n2 > n)) ? '' : this.path.slice(n + 1);
+			}
+		});
+		fsIterable.count[item.fileType]++;
+		log('createItem(\'%s\'): this.count=%s', path, inspect(fsIterable.count));
+		return item;
+	}
+}
---
+++ FsIterable/test/index.js
@@ -0,0 +1,104 @@
+
+"use strict";
+const obj /*{ assignDefaults, inspect, promisify }*/ = require('../../obj');
+const assert = require('assert');
+const inspect = obj.inspect;//require('util').inspect;//makeInspect({ depth: 3, /*breakLength: 0,*/ compact: false });
+const promisify = require('util').promisify;
+const nodePath = require('path');
+const { exec } = ({ exec: promisify(require('child_process').exec) });
+const FsIterable = require('../index.js');
+
+const log = require('debug')('test/FsIterable');//.extend('log');// log.log = (innerLog => ((...args) => (innerLog('prefix', ...args))))(console.log.bind(console));
+
+var mochaAsync = fn => fn;
+// {
+//     return async () => {
+//         try {
+//             return await fn();
+//         } catch (err) {
+//             log(` ! Error: ${err.stack||err}`);
+//         }
+//     };
+// };
+
+describe("FsIterable instance unit test", function() {
+
+	const exePath = nodePath.dirname(__filename);
+	const testPath = nodePath.join(exePath, './__testdata__');
+	let fsIterable;
+
+	log('FsIterable unit tests setup: testPath=\'%s\'\n\nassert=%s\n\n', testPath, inspect(assert));
+
+	//can't because then iter could be created and start enumerating FS before creating test files
+	beforeEach('create fsIterable', async function createFsIterable() {
+		// fsIterable = new FsIterable(testPath);
+		await exec(`rm -rf ${testPath} ; mkdir ${testPath}`);
+	});
+
+	it('should be of type FsIterable with count and progress properties', function properties() {
+		const fsIterable = new FsIterable(testPath);
+		assert.ok(fsIterable instanceof FsIterable && !!fsIterable.count && !!fsIterable.progress);
+	});
+
+	it('should be async iterable', async function iterable() {
+		const fsIterable = new FsIterable(testPath);
+		await assert.doesNotReject(async() => { for await (const i of fsIterable) {} });
+	});
+		
+	it('should yield 1 items for an empty directory walk', async function walkEmpty() {
+		const fsIterable = new FsIterable(testPath);
+		await assert.doesNotReject(async() => {
+			for await (let fsItem of fsIterable) {
+				log(`fsItem: Progress = ${(100 * fsIterable.itemIndex / fsIterable.count.all).toFixed(1)}% fsIterable.progress=${inspect(fsIterable.progress)}fsItem='${inspect(fsItem/*.path*/)}'`);//${inspect(fsItem)}`);
+			}
+		});
+		log(`Done: Count: ${inspect(fsIterable.count)} Progress=${inspect(fsIterable.progress)}`);
+		assert.equal(fsIterable.count.all, 1);
+	});
+
+	it('shold yield 2 items for a directory with 1 file walk', async function walk1File() {
+		await exec(`touch ${testPath}/__testfile__`);
+		const fsIterable = new FsIterable({ path: testPath, maxDepth: 4 });
+		await assert.doesNotReject(async() => {
+			for await (let fsItem of fsIterable) {
+				log(`fsItem: Progress = ${(100 * fsIterable.itemIndex / fsIterable.count.all).toFixed(1)}% fsIterable.progress=${inspect(fsIterable.progress)}fsItem='${inspect(fsItem/*.path*/)}'`);//${inspect(fsItem)}`);
+			}
+		});
+		log(`Done: Count: ${inspect(fsIterable.count)} Progress=${inspect(fsIterable.progress)}`);
+		assert.equal(fsIterable.count.all, 2);
+	});
+
+
+	it('shold yield 9 items for a directory with 8 file walk', async function walk8Files() {
+		await exec(`touch ${testPath}/__testfile__1`);
+		await exec(`touch ${testPath}/__testfile__2`);
+		await exec(`touch ${testPath}/__testfile__3`);
+		await exec(`touch ${testPath}/__testfile__4`);
+		await exec(`touch ${testPath}/__testfile__5`);
+		await exec(`touch ${testPath}/__testfile__6`);
+		await exec(`touch ${testPath}/__testfile__7`);
+		await exec(`touch ${testPath}/__testfile__8`);
+		const fsIterable = new FsIterable({ path: testPath, maxDepth: 4 });
+		await assert.doesNotReject(async() => {
+			for await (let fsItem of fsIterable) {
+				log(`fsItem: Progress = ${(100 * fsIterable.itemIndex / fsIterable.count.all).toFixed(1)}% fsIterable.progress=${inspect(fsIterable.progress)}fsItem='${inspect(fsItem/*.path*/)}'`);//${inspect(fsItem)}`);
+			}
+		});
+		log(`Done: Count: ${inspect(fsIterable.count)} Progress=${inspect(fsIterable.progress)}`);
+		assert.equal(fsIterable.count.all, 9);
+	});
+
+});
+
+process.once('SIGINT', onSigInt);
+function onSigInt() {
+	log(`fsIterable: ${inspect(fsIterable)}`);
+	process.once('SIGINT', quitHandler);
+	setTimeout(() => {
+			process.off('SIGINT', quitHandler);
+			process.once('SIGINT', onSigInt);
+		}, 1000);
+	function quitHandler() {
+		process.nextTick(() => process.exit(0));
+	}
+};
