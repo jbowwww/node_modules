b6a2930da81c56b83fbe5647481456fba5533a5f
Comment <jbowwww@gmail.com>
2019-08-22 21:55:01 +0800

    Added function call rate limiter module

--- Limit/index.js
+++ Limit/index.js
@@ -6,22 +6,16 @@ const inspect = obj.inspect;
 module.exports = Limit;
 
 function Limit(func, concurrency = 1) {
-	if (!(this instanceof Limit)) {
-		return new Limit(options);
-	} else if (typeof options === 'string') {
-		options = { path: options };
-	}
-	
 	let pendingPromises = [];
 	return async function (...args) {
-		while (pendingPromises.length >= n) {
+		while (pendingPromises.length >= concurrency) {
 		    await Promise.race(pendingPromises).catch(() => {});
 		}
 
-	    const p = fn.apply(this, args);
+	    const p = func.apply(this, args);
 		pendingPromises.push(p);
 		await p.catch(() => {});
 		pendingPromises = pendingPromises.filter(pending => pending !== p);
 		return p;
   	};
-|}
+}
