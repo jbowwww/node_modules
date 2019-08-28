3a06dc486ec499cbcc2245063a7ef573762345bb
Comment <jbowwww@gmail.com>
2019-08-28 20:47:19 +0800

    Convert Limit's pending array to be a property on the function so caller can access

--- Limit/index.js
+++ Limit/index.js
@@ -6,16 +6,18 @@ const inspect = obj.inspect;
 module.exports = Limit;
 
 function Limit(func, concurrency = 1) {
-	let pendingPromises = [];
-	return async function (...args) {
-		while (pendingPromises.length >= concurrency) {
-		    await Promise.race(pendingPromises).catch(() => {});
+	async function limitedFunction(...args) {
+		while (limitedFunction.pending.length >= concurrency) {
+		    await Promise.race(limitedFunction.pending).catch(() => {});
 		}
 
 	    const p = func.apply(this, args);
-		pendingPromises.push(p);
+		limitedFunction.pending.push(p);
 		await p.catch(() => {});
-		pendingPromises = pendingPromises.filter(pending => pending !== p);
+		limitedFunction.pending = limitedFunction.pending.filter(pending => pending !== p);
 		return p;
   	};
+  	limitedFunction.pending = [];
+  	Object.defineProperty(limitedFunction, 'allCompleted', { get: () => Promise.all(limitedFunction.pending) });
+  	return limitedFunction;
 }
