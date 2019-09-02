44a96bd8843e58bad1bf4bf98eb4c7f5651745e8
Comment <jbowwww@gmail.com>
2019-09-02 23:19:45 +0800

    Trying destructuring for swap operation

--- Limit/index.js
+++ Limit/index.js
@@ -4,19 +4,20 @@ const obj /*{ assignDefaults, inspect, promisify }*/ = require('../obj');
 const inspect = obj.inspect;
 
 module.exports = function Limit(fn, options = { }) {
-	options = obj.assignDefaults(options, { concurrency: 1 });
 	if (typeof fn !== 'function' && typeof options === 'function') {
-		let tmp = fn;
-		fn = options;
-		options = fn;
+		[ fn, options ] = [ options, fn ];
+		// let tmp = fn;
+		// fn = options;
+		// options = fn;
 	}
+	options = obj.assignDefaults(options, { concurrency: 1 });
 	const limitedFunction = async function(...args) {
 		while (limitedFunction.pending.length >= options.concurrency) {
 		    await Promise.race(limitedFunction.pending).catch(() => {});
 		}
 	    const p = fn.apply(this, args);
 		limitedFunction.pending.push(p);
-		await p.catch(() => {});
+		await p;//.catch(() => {});
 		limitedFunction.pending = limitedFunction.pending.filter(pending => pending !== p);
 		return p;
   	};
