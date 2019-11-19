6241f152b942e368f5041245c029b1f1fb4ee001
Comment <jbowwww@gmail.com>
2019-11-20 07:47:25 +0800

    Allow first parameter to be an array of inputs as well as just an input source

--- Source/index.js
+++ Source/index.js
@@ -1,19 +1,56 @@
-module.exports = (input, options = {}) => {
-	let next = null;
-	if (input[Symbol.iterator]) {
-		const it = input[Symbol.iterator]();
-		next = it.next.bind(it);
-	} else if (input[Symbol.asyncIterator]) {
-		const it = input[Symbol.asyncIterator]();
-		next = it.next.bind(it);
-	} else if (input.addEventListener || input.on || input.once) {
-		next = () => new Promise((resolve, reject) => {
-			input.once(options.event||'data', (data) => resolve({ value: data, done: false})).once('end', () => resolve({ done: true })).once('error', reject); });
+const Limit = require('@jbowwww/limit');
+
+// TODO: Finish combining 'source' mopdule into this one by allowing 'input' param to be an array of sources instead of just a source
+// definitely needs unit tests
+module.exports = (inputs, options = {}, fn) => {
+	const defaultOptions = { concurrency: 1 };
+	if (typeof options === 'function' && fn === undefined) {
+		fn = options;
+		options = defaultOptions;
+	} else if (typeof options === 'object') {
+		options = { ...defaultOptions, ...options };// Object.assign({ }, defaultOptions, options);
 	} else {
-		console.log(`Could not get source iteration technique!`);
+		throw new TypeError(`options should be an object`);
+	}
+	if (!!fn) {
+		if (typeof fn !== 'function') {
+			throw new TypeError(`fn should be a function`);
+		}
+		if (typeof options.concurrency === 'number') {
+			fn = Limit(fn);
+		}
+	}
+	const p = [];
+	for (let input of (!inputs.length ? [ inputs ] : inputs)) {
+		let next = null;
+		if (input[Symbol.asyncIterator]) {
+			const it = input[Symbol.asyncIterator]();
+			next = it.next.bind(it);
+		} else if (input[Symbol.iterator]) {
+			const it = input[Symbol.iterator]();
+			next = it.next.bind(it);
+		} else if (input.addEventListener || input.on || input.once) {
+			next = () => new Promise((resolve, reject) => {
+				input.once(options.event||'data', (data) => resolve({ value: data, done: false})).once('end', () => resolve({ done: true })).once('error', reject); });
+		} else {
+			console.log(`Could not get source iteration technique!`);
+		}
+		// Object.defineProperty(input, '_next', { enumerable: false, writeable: false, configurable: false, value: next });
+		(function pushPr(next) {
+			let pr = Promise.resolve(typeof fn === 'function' ? fn.call(input, next()) : next());
+			p.push(pr);
+			pr.then(({ done, val }) => {
+				p.splice(p.indexOf(pr));
+				if (!done) {
+					pushPr();
+				}
+			});
+		})(next);
 	}
 	const r = {
-		next,
+		next: async () => {
+			return p.length > 0 ? Promise.race(p) : Promise.resolve({ done: true });
+		},
 		[Symbol.iterator]() { return this; },
 		[Symbol.asyncIterator]() { return this; } 
 	};
