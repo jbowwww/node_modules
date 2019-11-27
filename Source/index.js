ba9fd96cec0d29a072629db4b74ab5ca476581cf
Comment <jbowwww@gmail.com>
2019-11-28 04:16:05 +0800

    Simplify source()

--- Source/index.js
+++ Source/index.js
@@ -2,23 +2,20 @@ const Limit = require('@jbowwww/limit');
 
 // TODO: Finish combining 'source' mopdule into this one by allowing 'input' param to be an array of sources instead of just a source
 // definitely needs unit tests
-module.exports = (inputs, options = {}, fn) => {
-	const defaultOptions = { concurrency: 1 };
-	if (typeof options === 'function' && fn === undefined) {
-		fn = options;
-		options = defaultOptions;
-	} else if (typeof options === 'object') {
-		options = { ...defaultOptions, ...options };// Object.assign({ }, defaultOptions, options);
-	} else {
-		throw new TypeError(`options should be an object`);
-	}
-	if (!!fn) {
-		if (typeof fn !== 'function') {
-			throw new TypeError(`fn should be a function`);
-		}
-		if (typeof options.concurrency === 'number') {
-			fn = Limit(fn);
-		}
+module.exports = source;
+
+function source(...inputs) {
+	const isOptionsArg = (arg) => typeof arg === 'object'
+		 &&	!arg[Symbol.asyncIterator]
+		 && !arg.addEventListener
+		 && !arg.on
+		 && !arg.once;
+	if (inputs.length === 0) throw new TypeError(`source needs to have >0 input parameters`);
+	let options = { event: 'data', endEvent: 'end' };
+	if (isOptionsArg(inputs[0])) {
+		options = Object.assign({}, options, inputs.shift());
+	} else if (isOptionsArg(inputs[inputs.length - 1])) {
+		options = Object.assign({}, options, inputs.pop());
 	}
 	let p = new Set();
 	for (let input of (!inputs.length ? [ inputs ] : inputs)) {
@@ -31,29 +28,46 @@ module.exports = (inputs, options = {}, fn) => {
 			next = it.next.bind(it);
 		} else if (input.addEventListener || input.on || input.once) {
 			next = () => new Promise((resolve, reject) => {
-				input.once(options.event||'data', (data) => resolve({ value: data, done: false})).once('end', () => resolve({ done: true })).once('error', reject); });
+				input
+				.once(options.event||'data', (data) => resolve({ value: data, done: false}))
+				.once(options.endEvent||'end', () => resolve({ done: true })).once('error', reject);
+			});
 		} else {
 			console.log(`Could not get source iteration technique!`);
 		}
 		// Object.defineProperty(input, '_next', { enumerable: false, writeable: false, configurable: false, value: next });
 		(function pushPr(next) {
 			// wrapping in promise should allow this to handle async and non-async generators?
-			let pr = Promise.resolve(typeof fn === 'function' ? fn.call(input, next()) : next());
+			let pr = Promise.resolve(next());
 			p.add(pr);
 			pr.then(({ done, val }) => {
 				p.delete(pr);
 				if (!done) {
-					pushPr();
+					pushPr(next);
 				}
 			});
 		})(next);
 	}
 	const r = {
 		next: async () => {
-			return p.size > 0 ? Promise.race(p) : Promise.resolve({ done: true });
+			return p.size > 0 ? Promise.race(p)/*.then(value => ({ value }))*/ : Promise.resolve({ done: true });
 		},
-		[Symbol.iterator]() { return this; },
-		[Symbol.asyncIterator]() { return this; } 
+		// [Symbol.iterator]() { return this; },
+		[Symbol.asyncIterator]() { return this; },
+		async pipe(...fns) {
+			for await (let data of this) {
+				for (const fn of fns) {
+					data = await fn(data);
+				}
+				// yield data;
+			}
+		}
 	};
 	return r;
-;}
+}
+
+source.pipe = sourcePipe;
+
+async function sourcePipe(...args) {
+	return await source(...args).pipe();
+}
