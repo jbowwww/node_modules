107a75d27f812c2a7bd439efd08e441dfd4d6bdd
Comment <jbowwww@gmail.com>
2019-11-28 04:15:04 +0800

    Tidy up ClusterProcesses

--- ClusterProcesses/index.js
+++ ClusterProcesses/index.js
@@ -4,12 +4,15 @@ const debug = require('debug')('cluster-processes');
 const { inspect } = require('util');
 const pEvent = require('p-event');
 
-module.exports = async (...processes) => {
-	const workerPromises = [];
+module.exports = clusterProcesses;
+
+async function clusterProcesses(...processes) {
+	const isNumber = value => typeof value === 'number' || /^[-+]?(\d+|Infinity)(\.\d*)?$/.test(value);const workerPromises = [];
 	if (cluster.isMaster) {
-		for (const i in processes) {
-			debug(`Master forking worker #${i}`);
-			const worker = cluster.fork({ id: i });
+		for (let processIndex in processes) {
+			// debug(`Master forking worker for processIndex='${processIndex}'`);
+			const worker = cluster.fork({ processIndex });
+			debug(`Master forked worker #${worker.id} for processIndex=${processIndex}`);
 			workerPromises.push(pEvent(worker, 'exit'));
 		}
 		debug(`Master awaiting ${workerPromises.length} promises: ${inspect(workerPromises)}`);
@@ -17,17 +20,20 @@ module.exports = async (...processes) => {
 		debug(`Master received fulfilment array of: ${inspect(ret)}`);
 	}
 	else if (cluster.isWorker) {
-		const id = process.env.id || -1; // cluster.worker.id;
-		if (id < 0) {
-			throw new Error(`Worker didn't get valid ID`);
-		}
-		debug(`worker process #${id} has processes: ${inspect(processes)}`);
+		const id = cluster.worker.id;
+		debug(`Worker #${id} has processes=${inspect(processes)}`);
+		const processIndex = process.env.processIndex;
+		debug(`Worker #${id} has processIndex=${processIndex}`);
+		const processFn = processes[processIndex] || processes[id];
+		debug(`Worker #${id} has processFn: ${inspect(processFn)}`);
+		const name = isNumber(processIndex) ? (processFn.name || `Worker #${processIndex}`) : processIndex;
+		debug(`Worker #${id} has name='${name}'`);
 		let ret;
 		try {
-			ret = await (processes[id])();
+			ret = await processFn();
+			debug(`worker process #${id} '${name}' returned: ${inspect(ret)}`);
 		} catch(e) {
-			debug(`worker process #${id} exception: ${e.stack||e}`);			
+			debug(`worker #${id} '${name}' exception: ${e.stack||e}`);			
 		}
-		debug(`worker process #${id} returned: ${inspect(ret)}`);
 	}
-};
+}
