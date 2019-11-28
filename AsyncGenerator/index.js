745e370259de6fc67a0b472fc24fe2c1f25b30d2
Comment <jbowwww@gmail.com>
2019-11-28 19:29:21 +0800

    Old changes made to AsyncGenerator

--- AsyncGenerator/index.js
+++ AsyncGenerator/index.js
@@ -4,22 +4,23 @@ const obj = require('@jbowwww/object-util');
 const log = require('debug')('asyncGenerator');
 const inspect = require('util').inspect;//utility.js').makeInspect({ depth: 3, compact: true });
 
-const AsyncGeneratorFunction = Object.getPrototypeOf(async function*(){}).constructor;
-log('asyncGenerator: ${inspect(AsyncGeneratorFunction)}');
+// TODO: Make a wrapper class/fn that encapsulates an async gen, giving it a this object, and defining tap,map,etc funcs
+// use would be like AsyncGenerator(async function* generatorFn() { }).map().filter().tap().pipe().run()
+const AsyncGeneratorPrototype = Object.getPrototypeOf(async function*(){});//.constructor;
 
 // TODO: Unit tests.
 
 // Not entirely sure if this will work on any old async function*
 // generator declared somewhere in code (with/without require()ing this file)
 // or whether such a generator would need to be written as a class that inherits from 
-// AsyncGeneratorFunction, or something else, or what ..?
+// AsyncGeneratorPrototype, or something else, or what ..?
 
 // TODO: tap, map, filter should also be exposed as standalone functoins (not on a prototype),
 // so they can be used inside other chains (not sure though if these standalone funcs
 // should return async functions, or async function*s, ..? maybe either, maybe depending on context or not ..?)
 
 // Tap forms a nested chain, awaiting its own completeion before resolving with the original unchanged item. 
-AsyncGeneratorFunction.prototype.tap = async function* tap(tapFn) {
+AsyncGeneratorPrototype.tap = async function* tap(tapFn) {
 	for await (const item of this) {
 		await tapFn(item);
 		yield item;
@@ -27,22 +28,22 @@ AsyncGeneratorFunction.prototype.tap = async function* tap(tapFn) {
 };
 
 // Dup is like tap, but doesn't await it's own completeion, so its inner chain runs simultaneously
-AsyncGeneratorFunction.prototype.dup = async function* dup(dupFn) {
+AsyncGeneratorPrototype.dup = async function* dup(dupFn) {
 	for await (const item of this) {
-		await tapFn(item);
+		dupFn(item);
 		yield item;
 	}
 };
 
 // standard map
-AsyncGeneratorFunction.prototype.map = async function* map(mapFn) {
+AsyncGeneratorPrototype.map = async function* map(mapFn) {
 	for await (const item of this) {
-		yield await mapFn(item);
+		yield await mapFn(item) || item;
 	}	
 };
 
 // standard filter
-AsyncGeneratorFunction.prototype.filter = async function* filter(filterFn) {
+AsyncGeneratorPrototype.filter = async function* filter(filterFn) {
 	for await (const item of this) {
 		if (await filterFn(item)) {
 			yield item;
@@ -61,54 +62,45 @@ AsyncGeneratorFunction.prototype.filter = async function* filter(filterFn) {
 // and then iterate this, passing each value to the iterable/ator's next()
 // * No wait scratch that, the caller should call the generator() and toObject
 // will be the iterable/ator instance
-AsyncGeneratorFunction.prototype.pipe = async function* pipe(toObject) {
-	for await (let item of this) {
-		await toObject.next(item);	// the writeable iterable shuld yield a promise that resolves when it's ready for another value
+AsyncGeneratorPrototype.pipe = async function* pipe(...pipeline) {
+	try {
+		log(`pipeline=${inspect(pipeline)} this=${inspect(this)}`);
+		try {
+			let itemCount = 0;
+			for await (let item of this) {
+				let stageCount = 0;
+				log(`item #${++itemCount}: ${inspect(item)}`);
+				for (let stage of pipeline) {
+					item = await stage(item);
+					log (`\tstage #${++stageCount} ${stage.name}: ${inspect(item)}`);
+				}
+				yield item;
+			}
+		} catch (e) {
+			log(`Error Inner, continuing: ${e.stack||e}`);
+		}
+	} catch (e) {
+		log(`Error Outer, stopping: ${e.stack||e}`);
 	}
 	// what about errors, exceptions, warnings ..?? closing (e.g. on writeable file)?
 };
 
 // set options (neater than parsing ...args)
-AsyncGeneratorFunction.prototype.options = function options(options) {
+AsyncGeneratorPrototype.options = function options(options) {
 
 };
 
 // run a 
-AsyncGeneratorFunction.prototype.run = async function run(options) {
+AsyncGeneratorPrototype.run = async function run(options) {
+	for await (let item of this) {
 
-};
-
-AsyncGeneratorFunction.prototype.pipe = async function (...pipeline) {
-	log(`pipeline=${inspect(pipeline)} this=${inspect(this)}`);
-	// const pipe = Array.prototype.reverse.call(pipeline);
-	// if (!pipeline.every(stage =>
-	// 	(obj.isPlain(stage) && obj.isFunction(stage[Symbol.asyncIterator]))
-	//  ||	(/*obj.isGenerator(stage) ||*/ obj.isAsyncGenerator(stage)) )) {
-	// 	throw new TypeError(`AsyncGenerator.prototype.pipe(): a stage is not an iterator or generator`);
-	// }
-	let iter = pipeline.reduce(async (acc, stage) => {
-		log(`stage: ${stage} acc=${acc} isAG=${obj.isAsyncGenerator(stage)} isAI=${obj.isAsyncIterable(stage)} this=${this}`);
-		return obj.isAsyncGenerator(stage) ? stage : async func* {
-			for await (const input of acc) {
-				yield map(input);
-			}
-		})(f);		/*.bind(acc)*//*(f));//.*/call(stage, acc);// obj.isAsyncGenerator(stage) ? stage(acc) : obj.isAsyncIterable(stage) ? stage(acc) : obj.isAsyncFunction(stage) ? stage(acc) : stage;
-	}, this);
-	log(`pipeline=${inspect(pipeline)} iter=${inspect(iter)} iter=${iter} this=${/*inspect*/(this)}`);
-	this._iter = iter;
-	// await iter;
-	// let done = false;
-	// while (!done) {
-	// 	let value = iter.next()
-	// }
-	for await (const item of await iter/*(this)*/) {
-		log(`item=${inspect(item)}`);
-		// yield item;
 	}
-	// return await iter;
 };
 
-class AsyncGenerator extends AsyncGeneratorFunction {
+log(`asyncGenerator: ${inspect(AsyncGeneratorPrototype)}`);
+
+class AsyncGenerator extends AsyncGeneratorPrototype.constructor {
+
 	constructor() {
 		super();
 		// this.pipe = AsyncGeneratorPipe;
@@ -119,13 +111,10 @@ class AsyncGenerator extends AsyncGeneratorFunction {
 		return this;
 	}
 
-	// async next(...args) {
-	// 	return _this.iter.next(...args);
-	// } 
 }
 
 
 module.exports = {
 	AsyncGenerator,
-	AsyncGeneratorFunction
+	AsyncGeneratorPrototype
 };
---
+++ AsyncGenerator/test/index.js
@@ -0,0 +1,29 @@
+
+"use strict";
+const obj = require('../../obj');
+const inspect = obj.inspect;//require('util').inspect;//makeInspect({ depth: 3, /*breakLength: 0,*/ compact: false });
+const log = require('debug')('AsyncGenerator/test');
+const assert = require('assert');
+
+describe("AsyncGenerator unit tests", function() {
+
+const { AsyncGenerator } = require('../');//@jbowwww/async-generator');
+
+	log(`AsyncGenerator unit tests setup\n\n\tassert methods: ${obj.keys(assert).filter(k => obj.isFunction(assert[k])).join(' ')}\n`);
+
+	beforeEach('', async function beforeEach() { });
+
+	// it('AsyncGenerator instance should have methods tap, map, filter, dup', function () {
+	// 	let iter = new AsyncGenerator();
+	// 	log(`iter = new AsyncGenerator() = ${inspect(iter)} / ${iter} / ${obj.keys(iter)}`);
+	// 	assert.equal(obj.namesExist(iter, 'tap', 'map', 'filter', 'dup'), true);
+	// });
+	
+	it('Instance of async function* () {} should have methods tap, map, filter, dup', function () {
+		let iter = AsyncGenerator((async function* () {})());
+		log(`iter = (async function* ())() = ${inspect(iter)} / ${iter} / ${obj.keys(iter)}`);
+		assert.ok(obj.isFunction(iter.tap));
+		assert.equal(obj.namesExist(iter, 'tap', 'map', 'filter', 'dup'), true);		
+	});
+
+});
