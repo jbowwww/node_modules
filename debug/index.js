fb3e4a913ddb200778eb133fc185d46811bc5ece
Comment <jbowwww@gmail.com>
2019-11-28 19:25:36 +0800

    debug wrapper that prepends log messages with cluster.worker.id/name when present

--- debug/index.js
+++ debug/index.js
@@ -1,8 +1,9 @@
 const debug = require('debug');
+const cluster = require('cluster');
 
 module.exports = customDebug;
 
 function customDebug(name) {
 	const innerDebug = debug(name);
-	return (...args) => innerDebug(`${cluster.isWorker?'#'+cluster.worker.id+' ':''}`, ...args);
+	return (...args) => innerDebug(`${cluster.isWorker?'#'+cluster.worker.id+' '+cluster.worker.name+' ':''}${args.shift()}`, ...args);
 }
