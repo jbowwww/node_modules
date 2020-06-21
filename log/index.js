7b0112171026c90167c433400a29a7d818baa751
Joel Bowman <=>
2020-06-22 01:14:46 +0800

    Add log implmmentation

--- log/index.js
+++ log/index.js
@@ -1,21 +1,56 @@
 const debug = require('debug');
-
-const log = {
-	debug: debug('model/filesys/filesys-entry:debug'),
-	verbose: debug('model/filesys/filesys-entry:verbose'),
-	info: debug('model/filesys/filesys-entry:info'),
-	warn: debug('model/filesys/filesys-entry:warn'),
-	error: debug('model/filesys/filesys-entry:error')
-};
-log.log = log.info;
+const path = require('path');
+const cluster = require('cluster');
 
 function _log(...args) {
 	return log.info(...args)
 };
 
-for (const level of log) {
-	_log[level] = log[level];
-}
+const fileName = module.parent.filename.replace(new RegExp('^' + path.dirname(require.main.filename)), '');
+const moduleName = fileName.replace(/\.js$/i, '');
+console.log({ __filename, __dirname, requireMainFilename: require.main.filename, fileName, moduleName });
+delete require.cache[__filename];	// delete so module.parent repopulated at next require()
 
-module.exports = _log;
+const log = {
+	debug: debug(`${moduleName}:debug`),
+	verbose: debug(`${moduleName}:verbose`),
+	info: debug(`${moduleName}:info`),
+	warn: debug(`${moduleName}:warn`),
+	error: debug(`${moduleName}:error`)
+};
+log.log = log.info;
+
+// for (const p of Object.keys(global.Object)) {
+// 	console.log(`Object.${p}: ${Object[p]}`);
+// }
+// console.log(`Object.entries: ${Object.entries}\nObject.fromEntries: ${Object.fromEntries}\nObject props: ${Object.keys(Object)}`);
+// const debug = require('debug');
+// function _outerLog(...args) {
+	const r = Object.assign(_log, {
+		...({
+			__config: {},
+			config() { return this.__config; },
+			enabled(level) { return !this.__config.hasOwnProperty(level) || this.__config[level]; },
+			enable(level, enabled = true) { this.__config[level] = enabled; return this; },
+			disable(level) { this.__config[level] = false; return this; }
+		}),
+		...(Object.fromEntries([ 'debug', 'verbose', 'info', 'warn', 'error' ]
+		.map(level => [
+			level,
+			(...args) => r.enabled(level) && (debug(
+				(cluster.isWorker ? '#' + cluster.worker.id + ' ' + cluster.worker.name + ' ' : '')
+				+ `${moduleName}:${level}`)(...args))
+		]))),
+		log: this.info
+	});//.forEach(([level, logger]) => _log[level] = logger);
+	// if (typeof _log.info === 'function')
+	// 	_log.log = _log.info;
+	// return _log;
+	module.exports = r;
+	function _log(...args) {
+		return _log.info(...args)
+	}
+// }
+
+// module.exports = _outerLog;
 
